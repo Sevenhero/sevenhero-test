4c36a9cc6da9b45546678022e48dafed621ef521
unknown <sven.kernke@yahoo.de>
2023-07-05 21:07:20 +0200

    Added Dist

---
+++ dist/src/myClasses.d.ts
@@ -0,0 +1,3 @@
+export default class Testikus {
+    name: string;
+}
---
+++ dist/src/myClasses.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Testikus {
+    name;
+}
+exports.default = Testikus;
+//# sourceMappingURL=myClasses.js.map
